65d7716b07bff3e459378b3a03e2f74293fcc73e
Tatyana Pushkarskaya <t.pushkarskaya@inbox.ru>
2021-06-03 00:59:53 +0300

    CheckMessage

--- src/test/java/CheckMessage.java
+++ src/test/java/CheckMessage.java
@@ -1,6 +1,4 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,23 +6,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 
 public class CheckMessage {
 
     protected static WebDriver driver;
     private final Logger logger = LogManager.getLogger(CheckMessage.class);
-    // private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
 
     @BeforeClass
     public static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
     @Before
     public void setUp() {
         driver = new ChromeDriver();
         logger.info("Драйвер поднят");
-        logger.warn("Драйвер поднят");
     }
 
     @After
@@ -44,10 +43,17 @@ public class CheckMessage {
         getElement(button).click();
         String messageFirst = getElement(alert).getText();
 
+        logger.info("messageFirst");
+        logger.info(messageFirst);
+
         Thread.sleep(1500);
 
         getElement(button).click();
         String messageSecond = getElement(alert).getText();
+
+        logger.info("messageSecond");
+        logger.info(messageSecond);
+
 
         Assert.assertNotEquals(messageFirst, messageSecond);
     }
