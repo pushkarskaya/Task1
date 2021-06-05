447036c0a8499b7e6c7350e7c226bd45a193e98c
Tatyana Pushkarskaya <t.pushkarskaya@inbox.ru>
2021-06-06 01:24:15 +0300

    Добавление Owner

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -1,11 +1,10 @@
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.*;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
 
 public class Task1 {
     private WebDriver driver;
@@ -32,12 +31,13 @@ public class Task1 {
     public void test() {
         logger.info("Test start");
         String title="Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
-        driver.get("https://otus.ru/"); // Your test code here
+        ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+        System.out.println(cfg.hostname() + ":" + cfg.port() +
+                " will run " + cfg.maxThreads());
+        driver.get(cfg.hostname());
         driver.getTitle();
         String actual=driver.getTitle();
         Assert.assertEquals(title, actual);
-
-
         logger.info(actual);
     }
 }
