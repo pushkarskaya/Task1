da75fce8574c65ecb96305c1bd8698746c414448
Tatyana Pushkarskaya <t.pushkarskaya@inbox.ru>
2021-06-06 16:08:42 +0300

    Домашняя работа №3

---
+++ src/test/java/ConfigDriver.java
@@ -0,0 +1,102 @@
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import org.junit.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+public class ConfigDriver {
+    private WebDriver driver;
+    private Logger logger = LogManager.getLogger(ConfigDriver.class);
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void checkingAddress() {
+        logger.info("Старт теста");
+        driver.get("https://otus.ru");
+
+        String address = "125167, г. Москва, Нарышкинская аллея., д. 5, стр. 2, тел. +7 499 938-92-02";
+        logger.info("Загружена страница " + driver.getCurrentUrl());
+
+        WebElement contactElement = driver.findElement(By.cssSelector(".header2_subheader-link[href=\"/contacts/\"]"));
+        contactElement.click();//Нажата ссылка Контакты
+        WebElement actAddress= driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[4]/div[1]/div[3]/div[2]"));
+        logger.info(address);
+        logger.info(actAddress.getText());
+        Assert.assertEquals(address, actAddress.getText());
+        driver.manage().window().fullscreen();
+    }
+    @Test
+    public void checkingTitle(){
+        logger.info("Старт теста");
+        driver.get("https://otus.ru");
+
+        String title="Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
+        logger.info("Название страницы ожидание:"+title);
+        logger.info("Название страницы актуальное:"+driver.getTitle());
+        Assert.assertEquals(title,driver.getTitle());
+
+    }
+    @Test
+    public void Tele2() throws InterruptedException{
+        driver.get("https://msk.tele2.ru/shop/number");
+        WebElement numInput=driver.findElement(By.cssSelector("#searchNumber"));
+        logger.info("Поиск номера телефона,содержащего 97");
+        numInput.sendKeys("97");
+        Thread.sleep(2000);
+    }
+    @Test
+    public void FindCourse() {
+        driver.get("https://otus.ru");
+        WebElement faq = driver.findElement(By.cssSelector(".header2_subheader-link[href=\"/faq/\"]"));
+        faq.click();
+        WebElement question = driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[2]/div[2]/div[4]/div[1]"));
+        question.click();
+
+        String answer="Программу курса в сжатом виде можно увидеть на странице курса после блока с преподавателями. Подробную программу курса можно скачать кликнув на “Скачать подробную программу курса”";
+        WebElement answerElement=driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[2]/div[2]/div[4]/div[2]"));
+
+        logger.info("Ответ ожидаемый" + answer);
+        logger.info("Ответ актуальный" +answerElement.getText());
+        Assert.assertEquals(answer, answerElement.getText());
+    }
+
+    @Test
+    public void Subscription(){
+        driver.get("https://otus.ru");
+        String inputMail="test1@mail.ru";
+        WebElement subscribeElement= driver.findElement(By.cssSelector(".input.footer2__subscribe-input[name=email]"));
+        subscribeElement.sendKeys(inputMail);
+        WebElement subscribeButtonElement=driver.findElement(By.cssSelector(".footer2__subscribe-button"));
+        subscribeButtonElement.click();
+        WebElement sucsessSubscribeElement=driver.findElement((By.cssSelector(".subscribe-modal__success")));
+        String sucsess="Вы успешно подписались";
+        Assert.assertEquals(sucsess, sucsessSubscribeElement.getText());
+        logger.info("Ожидаемое сообщение после нажатия \"Подписаться \""+sucsess);
+        logger.info("Актуальное сообщение после нажатия \"Подписаться \""+sucsessSubscribeElement.getText());
+    }
+}
+
